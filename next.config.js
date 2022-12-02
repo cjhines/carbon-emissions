d039c1f14845989a623fd19f8267a8a07c4cd44a
cjhines <christopher.hines@hybridheroes.de>
2022-12-02 11:40:45 +0100

    deployment attempt

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: isProd ? '/carbon-emissions/' : '',
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
